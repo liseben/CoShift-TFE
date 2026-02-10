e62139441da03ca137c11ba5f9063b67f8e10fa0
Krefel <elisabethbenga05@gmail.com>
2026-02-10 01:32:12 +0100

    feat(backend): Init Spring Boot with Security and Database config

---
+++ coshift-backend/src/main/java/com/coshift/api/CoshiftBackendApplication.java
@@ -0,0 +1,13 @@
+package com.coshift.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoshiftBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoshiftBackendApplication.class, args);
+	}
+
+}
---
+++ coshift-backend/src/test/java/com/coshift/api/CoshiftBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coshift.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoshiftBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
