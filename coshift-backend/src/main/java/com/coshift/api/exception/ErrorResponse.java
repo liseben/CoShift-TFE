e608d4c2ba46b34876d72b0f7de703ef640b5952
Krefel <elisabethbenga05@gmail.com>
2026-03-10 14:25:04 +0100

    création d'une classe de réponse d'érreur(le DTO d'erreur)

---
+++ coshift-backend/src/main/java/com/coshift/api/exception/ErrorResponse.java
@@ -0,0 +1,11 @@
+package com.coshift.api.exception;
+
+import java.time.LocalDateTime;
+
+public record ErrorResponse(
+  LocalDateTime timestamp,
+    int status,
+    String error,
+    String message,
+    String path
+) {}
