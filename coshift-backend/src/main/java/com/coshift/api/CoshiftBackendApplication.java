62825d13c4b522d77e0a08cd733dec8b6f1b3886
Krefel <elisabethbenga05@gmail.com>
2026-03-17 11:53:26 +0100

    feat(backend): activation du scheduling global pour l'automatisation

--- coshift-backend/src/main/java/com/coshift/api/CoshiftBackendApplication.java
+++ coshift-backend/src/main/java/com/coshift/api/CoshiftBackendApplication.java
@@ -3,9 +3,12 @@ package com.coshift.api;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 import java.io.File;
 
 @SpringBootApplication
+@EnableScheduling
 public class CoshiftBackendApplication {
 
     public static void main(String[] args) {
